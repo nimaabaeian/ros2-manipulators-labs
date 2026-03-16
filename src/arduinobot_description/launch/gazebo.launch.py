0d0ef30794c0bbeaf93b291fb34d6c544a408f05
nimaabaeian <abaeiannima@gmail.com>
2026-03-16 18:04:16 +0100

    Step 6 added

---
+++ src/arduinobot_description/launch/gazebo.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from pathlib import Path
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    arduinobot_description_dir = get_package_share_directory("arduinobot_description")
+    model_arg = DeclareLaunchArgument(
+        name="model", 
+        default_value=os.path.join(arduinobot_description_dir, "urdf", "arduinobot.urdf.xacro"),
+        description="Absolute path to robot urdf file"
+    )
+    gazebo_resource_path = SetEnvironmentVariable(
+        name= "GZ_SIM_RESOURCE_PATH",
+        value=str(Path(arduinobot_description_dir).parent.resolve())
+    )
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description, "use_sim_time": True}],
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments=[
+            ("gz_args", [" -v 4 -r empty.sdf"])
+        ]
+    )
+
+    gz_spawn_entity = Node(
+        package = "ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description", "-name", "arduinobot"]
+    )
+
+    gz_ros2_bridge = Node(
+        package = "ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"]
+    )
+
+    return LaunchDescription([
+        model_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        gz_ros2_bridge,
+        ])
