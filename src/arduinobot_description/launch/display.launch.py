16a62a49292a25df0b45f3c644372a2e533090f4
nimaabaeian <abaeiannima@gmail.com>
2026-04-06 20:56:03 +0200

    Step 8 added

--- src/arduinobot_description/launch/display.launch.py
+++ src/arduinobot_description/launch/display.launch.py
@@ -1,27 +1,32 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.parameter_descriptions import ParameterValue
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration
 
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
 def generate_launch_description():
-    model_arg = DeclareLaunchArgument(
-        name="model", 
-        default_value=os.path.join(get_package_share_directory("arduinobot_description"), "urdf", "arduinobot.urdf.xacro"),
-        description="Absolute path to robot urdf file"
-    )
+    arduinobot_description_dir = get_package_share_directory("arduinobot_description")
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        arduinobot_description_dir, "urdf", "arduinobot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file")
 
-    robot_state_publisher = Node(
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
+                                       value_type=str)
+
+    robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}],
+        parameters=[{"robot_description": robot_description}]
     )
 
-    joint_state_publisher_gui = Node(
+    joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui"
     )
@@ -31,13 +36,12 @@ def generate_launch_description():
         executable="rviz2",
         name="rviz2",
         output="screen",
-        arguments=["-d", os.path.join(get_package_share_directory("arduinobot_description"), "rviz", "display.rviz")],
-        parameters=[{"use_sim_time": True}]
+        arguments=["-d", os.path.join(arduinobot_description_dir, "rviz", "display.rviz")],
     )
 
     return LaunchDescription([
         model_arg,
-        robot_state_publisher,
-        joint_state_publisher_gui,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
         rviz_node
     ])
